ee5ccff1f51d23f17057418cea9df0a8116d8b7b
castleiMac <castle@tansuyun.cn>
2019-12-20 17:12:27 +0800

    改版成promise

--- src/index.ts
+++ src/index.ts
@@ -6,55 +6,48 @@ var rABS = typeof FileReader !== "undefined" && typeof FileReader.prototype !== 
 
 export const isBrower = 'undefined' !== typeof window;
 /**
- * 读出JSON为
+ * 读取Excel文件为JSON内容
  * @param file 
  * @param success 
  */
-export function readAsJSON(file: string | any, success?: (d: any) => void): any {
-    let Data: any = {};
-    if (isBrower) {
-        var reader = new FileReader();
-        reader.onload = function (e: any) {
-            var data = e.target.result;
-            let workbook = XLSX.read(data, { type: 'binary' })
+export function readAsJSON(file?: string | any): any {
+    return new Promise((s, j) => {
+        let Data: any = {};
+        if (isBrower) {
+            var reader = new FileReader();
+            reader.onload = function (e: any) {
+                var data = e.target.result;
+                let workbook = XLSX.read(data, { type: 'binary' })
+                workbook.SheetNames.forEach((d: string) => {
+                    Data[d] = XLSX.utils.sheet_to_json(workbook.Sheets[d])
+                })
+                s(Data)
+            }
+            if (rABS) reader.readAsBinaryString(file);
+            else reader.readAsArrayBuffer(file);
+        } else {
+            let workbook = XLSX.readFile(file)
             workbook.SheetNames.forEach((d: string) => {
                 Data[d] = XLSX.utils.sheet_to_json(workbook.Sheets[d])
             })
-            if (success instanceof Function) {
-                success(Data)
-            }
+            s(Data)
         }
-        if (rABS) reader.readAsBinaryString(file);
-        else reader.readAsArrayBuffer(file);
-    } else {
-        let workbook = XLSX.readFile(file)
-        workbook.SheetNames.forEach((d: string) => {
-            Data[d] = XLSX.utils.sheet_to_json(workbook.Sheets[d])
-        })
-        if (success instanceof Function) {
-            success(Data)
-        }
-    }
-    return Data;
+    })
 }
 /**
- * 
+ * 从JSON结构中生成XLSX文件
  * @param Data 
  * @param FileName 
  */
-export function writeFileFromJSON(Data: any, FileName: string) {
+export function writeFileFromJSON(Data: { [index: string]: { [index: string]: string | number | boolean }[] }, FileName: string) {
     let WorkBook = XLSX.utils.book_new()
     for (let x in Data) {
         XLSX.utils.book_append_sheet(WorkBook, XLSX.utils.json_to_sheet(Data[x]))
     }
-    if (isBrower) {
-        XLSX.writeFile(WorkBook, FileName)
-    } else {
-        XLSX.writeFile(WorkBook, FileName)
-    }
+    XLSX.writeFile(WorkBook, FileName)
 }
 /**
- * 
+ * 从Table标签中生成XLSX文件内容
  * @param id 
  * @param FileName 
  */
